0f9897c56c716e7bfb54088352e8656a03fb0b15
JorgeGWD <jorge.rodriguez.r@gmail.com>
2021-01-13 10:48:02 -0600

    Get API response, show images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,32 @@
-const fetchPhotos = () => {
+const fetchPhotos = async () => {
+
     const API_URL = 'https://api.unsplash.com/'
+
     const API_KEY = 'b1-Q2sdry5X8DLVH2zN5BOr5gLmk1lXZlY4_GvzlwK4'
 
-    fetch(API_URL+ 'photos/?client_id=' + API_KEY)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        // console.log(data)
+    let response = await fetch(API_URL+ 'photos/?client_id=' + API_KEY)
+    
+    let jsonResponse = await response.json()
 
-        data.forEach(photo => {
-            const result = photo.urls.small
-            
-            document.getElementById('img').append(result)
+    let imagesList = await jsonResponse
 
-            console.log(result)
-        });
-    })
-    .catch(error => alert(error))
+    console.log(imagesList)
+
+    const createImages = (imagesList) => {
+
+        for(let i = 0 ; i < imagesList.length ; i++ ) {
+
+        const image = document.createElement('img');
+
+        image.src = imagesList[i].urls.thumb;
+
+        document.body.appendChild(image);
+
+        }
+    }
+
+    createImages(imagesList)
+    
 }
 
 fetchPhotos()
